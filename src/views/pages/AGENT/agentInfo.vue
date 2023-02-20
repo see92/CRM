93298c9a76e8c63e1bed7f1fc663dec9845dd4c2
see92 <906437853@qq.com>
2023-02-20 23:32:04 +0800

    代理商信息

--- src/views/pages/AGENT/agentInfo.vue
+++ src/views/pages/AGENT/agentInfo.vue
@@ -1,13 +1,56 @@
 <template>
   <b-card>
-    <div class="d-flex">
+    <div class="d-flex mb-2">
       <feather-icon :icon="`${flag == 0 ? 'UserPlusIcon' : 'UserIcon'}` " size="19" />
       <h4 class="mb-0 ml-50">{{ flag==0?'新增代理商':'修改代理商' }}</h4>
     </div>
-
     <ValidationObserver ref="agentForm" #default="{invalid}">
-      <b-form @submit.prevent>
-
+      <b-form @submit.prevent class="form">
+        <b-row class="row">
+          <b-col cols="7">
+            <b-form-group label="代理商名称：" label-for="agentName" label-cols-md="auto">
+              <ValidationProvider rules="required" name="agentName" #default="{errors}">
+                <b-form-input maxlength="255" :state="errors.length>0?false:null" v-model="agent.name" class="w34" />
+                <small class="text-danger">{{errors[0]}}</small>
+              </ValidationProvider>
+            </b-form-group>
+          </b-col>
+          <b-col cols="7">
+            <b-form-group label="代理商规模：" label-for="size" label-cols-md="auto">
+              <v-select class="w34" />
+            </b-form-group>
+          </b-col>
+          <b-col cols="7">
+            <b-form-group label="行业分类：" label-for="fenLei" label-cols-md="auto">
+              <el-cascader class="w34" />
+            </b-form-group>
+          </b-col>
+          <b-col cols="7">
+            <b-form-group label="代理商等级：" label-for="leave" label-cols-md="auto">
+              <v-select class="w34" />
+            </b-form-group>
+          </b-col>
+          <b-col cols="7">
+            <b-form-group label="社会信用代码：" label-for="code" label-cols-md="auto">
+              <b-form-input class="w34" />
+            </b-form-group>
+          </b-col>
+          <b-col cols="7">
+            <b-form-group label="签约时间：" label-for="time" laebl-for="time" label-cols-md="auto">
+              <b-form-input class="w34" />
+            </b-form-group>
+          </b-col>
+          <b-col cols="7">
+            <b-form-group label="签约到期时间：" label-for="out_time" label-cols-md="auto">
+              <b-form-input class="w34" />
+            </b-form-group>
+          </b-col>
+          <b-col cols="7">
+            <b-form-group label="报备额度：" label-for="count" label-cols-md="auto">
+              <b-form-input class="w34" />
+            </b-form-group>
+          </b-col>
+        </b-row>
       </b-form>
     </ValidationObserver>
   </b-card>
@@ -16,6 +59,8 @@
 
 <script>
 import { ValidationObserver, ValidationProvider } from "vee-validate";
+import { required } from "@validations";
+
 export default {
   name: 'agentInfo',
   components: {
@@ -23,8 +68,18 @@ export default {
   },
   data() {
     return {
-      flag: this.$route.query.flag
+      required,
+      flag: this.$route.query.flag,
+      agent: {
+        name: ''
+      }
     }
   },
 }
 </script>
+
+<style>
+.w34 {
+  width: 34.375rem;
+}
+</style>
